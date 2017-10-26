5ed1fe25d6ecb47f9c2e866c551f92c69d7ac40e
David Schwartz <david.schwartzy@gmail.com>
2017-10-26 09:36:33 -0400

    cleaned up code

--- Lab 2/Program.cs
+++ Lab 2/Program.cs
@@ -11,8 +11,10 @@ namespace Lab_2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! This program will tell you if your input integer is even or odd.");
-            while (true)
+            bool rep = true;
+            while (rep)
             {
+
                 bool valid = false;
                 Console.WriteLine("What is your first name?");
                 string name = Console.ReadLine();
@@ -29,44 +31,15 @@ namespace Lab_2
                             if (answer > 1 && answer < 26)
                             {
                                 Console.WriteLine($"{name}, {answer} is even and less than 25.");
-                                Console.WriteLine("Would you like to run this program again? (Y/N)");
-                                var repeat = Console.ReadLine();
-                                if (repeat == "y" || repeat == "Y")
-                                {
-                                    valid = true;
-                                }
-                                else
-                                {
-                                    Environment.Exit(0);
-                                }
                             }
                             else if (answer >= 26 && answer <= 60)
                             {
                                 Console.WriteLine($"{name}, {answer} is even.");
-                                Console.WriteLine("Would you like to run this program again? (Y/N)");
-                                var repeat = Console.ReadLine();
-                                if (repeat == "y" || repeat == "Y")
-                                {
-                                    valid = true;
-                                }
-                                else
-                                {
-                                    Environment.Exit(0);
-                                }
                             }
                             else if (answer <= 100)
                             {
                                 Console.WriteLine($"{name}, {answer} is even.");
-                                Console.WriteLine("Would you like to run this program again? (Y/N)");
-                                var repeat = Console.ReadLine();
-                                if (repeat == "y" || repeat == "Y")
-                                {
-                                    valid = true;
-                                }
-                                else
-                                {
-                                    Environment.Exit(0);
-                                }
+                                
                             }
                         }
                         else if (answer % 2 != 0)
@@ -74,30 +47,27 @@ namespace Lab_2
                             if (answer > 60)
                             {
                                 Console.WriteLine($"{name}, {answer} is odd.");
-                                Console.WriteLine("Would you like to run this program again? (Y/N)");
-                                var repeat = Console.ReadLine();
-                                if (repeat == "y" || repeat == "Y")
-                                {
-                                    valid = true;
-                                }
-                                else
-                                {
-                                    Environment.Exit(0);
-                                }
                             }
                             else
                             {
                                 Console.WriteLine($"{name}, {answer} is odd.");
-                                Console.WriteLine("Would you like to run this program again? (Y/N)");
-                                var repeat = Console.ReadLine();
-                                if (repeat == "y" || repeat == "Y")
-                                {
-                                    valid = true;
                                 }
-                                else
-                                {
-                                    Environment.Exit(0);
-                                }
+                        }
+                        //asks if user would like to run program again
+                        Console.WriteLine("Would you like to run this program again? (Y/N)");
+                        string runagain = Console.ReadLine();
+
+                        while (success)
+                        {
+                            if (runagain == "y" || runagain == "Y")
+                            {
+                                Console.WriteLine($"Okay {name}, please enter an integer between 1 - 100(whole number).");
+                                rep = true;
+                                break;
+                            }
+                            else
+                            {
+                                return;
                             }
                         }
                     }
